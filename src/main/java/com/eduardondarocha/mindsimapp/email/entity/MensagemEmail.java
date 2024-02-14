9ab145e9050281446ad3d40cb522c949599f2594
enrocha0312 <enrocha0312@gmail.com>
2024-02-14 09:14:05 -0300

    modelo de mensagem de email

--- src/main/java/com/eduardondarocha/mindsimapp/email/entity/MensagemEmail.java
+++ src/main/java/com/eduardondarocha/mindsimapp/email/entity/MensagemEmail.java
@@ -1,4 +1,20 @@
 package com.eduardondarocha.mindsimapp.email.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.nio.file.Path;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MensagemEmail {
+    private String assunto;
+    private String texto;
+    private String remetente;
+    private List<String> destinatarios;
+    private Path caminhoArquivo;
+    private String nomeArquivo;
 }
